4fc84273d72dc073effdabea517b4b30f644fa85
Uli Heller <uli.heller@daemons-point.com>
2022-01-25 08:19:32 +0100

    Moved additionalElements

---
+++ step-17_config-js/config.js
@@ -0,0 +1,8 @@
+
+const config = {
+    additionalElements: [
+        { filename: "header.md",    elementId: "topid" },
+        { filename: "header-02.md", elementId: "topid" },
+        { filename: "footer.md",    elementId: "bottomid" },
+    ],
+}
