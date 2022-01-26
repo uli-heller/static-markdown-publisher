e59b75ff86a11714257cd41eadfa4d271e364fc5
Uli Heller <uli.heller@daemons-point.com>
2022-01-26 08:08:34 +0100

    Added elementId

--- step-22_left-right-navbar/config.js
+++ step-22_left-right-navbar/config.js
@@ -1,9 +1,9 @@
 
 const config = {
     additionalElements: [
-        { filename: "header.md", insertBeforeElementId: "topid" },
-        { filename: "navbar.md", insertBeforeElementId: "topid" },
-        { filename: "footer.md", insertBeforeElementId: "bottomid" },
+        { filename: "header.md", elementId: "headermd", insertBeforeElementId: "topid" },
+        { filename: "navbar.md", elementId: "navbarmd", insertBeforeElementId: "topid" },
+        { filename: "footer.md", elementId: "footermd", insertBeforeElementId: "bottomid" },
     ],
     stylesheets: [
 	"stuttgart.css",
