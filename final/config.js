8b4f0f1c11a0ea82156c770d7ab19ac93ff29581
Uli Heller <uli.heller@daemons-point.com>
2022-02-12 23:55:11 +0100

    step-30 -> final

--- final/config.js
+++ final/config.js
@@ -1,16 +1,17 @@
 
 const config = {
     additionalElements: [
-        { filename: "/header.md", elementId: "headermd", insertBeforeElementId: "topid" },
-        { filename: "/navbar.md", elementId: "navbarmd", insertBeforeElementId: "topid",    isNavbar: true },
-        { filename: "/footer.md", elementId: "footermd", insertBeforeElementId: "bottomid", isNavbar: true },
+        { filename: "header.md", elementId: "headermd", insertBeforeElementId: "topid" },
+        { filename: "navbar.md", elementId: "navbarmd", insertBeforeElementId: "topid",    isNavbar: true },
+        { filename: "footer.md", elementId: "footermd", insertBeforeElementId: "bottomid", isNavbar: true },
     ],
     stylesheets: [
-	"/stuttgart.css",
+	"stuttgart.css",
     ],
     multiLanguage: false,
     navbarClass:   'navbar',
     indexHtml:     'index.html',
+    indexMd:       'index.md',
     markdown:      [ '.md',   '.markdown' ],
     html:          [ '.html', '.htm' ],
     text:          [ '.txt' ],
