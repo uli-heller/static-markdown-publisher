4acfc0a8f4c63ae575f895cc53827f15dc08fef4
Uli Heller <uli.heller@daemons-point.com>
2022-02-12 23:50:30 +0100

    step-30: Identified and fixed second issue

--- step-30_welcome-page/README.md
+++ step-30_welcome-page/README.md
@@ -47,5 +47,25 @@ Lösung:
              base.href = mySite.currentUrl;
 ```
 
+### Unterordner
+
+Vorbereitungen:
+
+- Lege ein temporäres Verzeichnis an: `mkdir x`
+- Lege einen Symlink an: `ln -s ../step-30_welcome-page x/y`
+- Starte den Python-Server: `python3 -m http.server 8000 -d x/`
+- Greife mittels Browser zu auf <http://localhost:8000/y>
+- Stoppe den Python-Server
+- Lösche das temporäre Verzeichnis: `rm -rf x`
+
+Problem:
+
+- Die "index.md" wird korrekt angezeigt, die Kopf- und Fußzeilen fehlen!
+
+Lösung:
+
+- Anpassung 1: In "config.js" geben wir die Resourcen ohne führenden Schrägstrich an
+- Anpassung 2: Spezielle Ladefunktionen für Resourcen aus config.js einführen in "index.html"
+
 [MAIN]:  ../README.md
 
--- step-30_welcome-page/config.js
+++ step-30_welcome-page/config.js
@@ -1,12 +1,12 @@
 
 const config = {
     additionalElements: [
-        { filename: "/header.md", elementId: "headermd", insertBeforeElementId: "topid" },
-        { filename: "/navbar.md", elementId: "navbarmd", insertBeforeElementId: "topid",    isNavbar: true },
-        { filename: "/footer.md", elementId: "footermd", insertBeforeElementId: "bottomid", isNavbar: true },
+        { filename: "header.md", elementId: "headermd", insertBeforeElementId: "topid" },
+        { filename: "navbar.md", elementId: "navbarmd", insertBeforeElementId: "topid",    isNavbar: true },
+        { filename: "footer.md", elementId: "footermd", insertBeforeElementId: "bottomid", isNavbar: true },
     ],
     stylesheets: [
-	"/stuttgart.css",
+	"stuttgart.css",
     ],
     multiLanguage: true,
     navbarClass:   'navbar',
