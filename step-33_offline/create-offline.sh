d13145a9f575bbd008eac9d7f4a6335f0d2e1123
Uli Heller <uli.heller@daemons-point.com>
2023-04-02 08:37:41 +0200

    Skript-Tuning

--- step-33_offline/create-offline.sh
+++ step-33_offline/create-offline.sh
@@ -3,7 +3,8 @@
 D="$(dirname "$0")"
 BN="$(basename "$0")"
 
-TARGET=offline
+TARGET="$1"
+test -n "${TARGET}" || TARGET=offline
 
 test -d "${TARGET}" || mkdir "${TARGET}" || exit 1
 
@@ -12,16 +13,18 @@ for f in "${D}/"*; do
     test -f "${f}" && cp "${f}"  "${TARGET}/."
 done
 
-for u in $(grep '<script.*http' index.html|grep -v '<!--.*<script'|grep -o 'src="http[^"]*"'|cut -d"=" -f2-|tr -d '"'); do
+for u in $(grep '<script.*http' "${TARGET}/index.html"|grep -v '<!--.*<script'|grep -o 'src="http[^"]*"'|cut -d"=" -f2-|tr -d '"'); do
     BNU="$(basename "${u}")"
     rm -f "${TARGET}/${BNU}"
     ( cd "${TARGET}" && wget -q "${u}" )
     sed -i -e "s!${u}!${BNU}!g" "${TARGET}/index.html"
 done
 
-for u in $(grep '<link.*http' index.html|grep -v '<!--.*<link'|grep -o 'href="http[^"]*"'|cut -d"=" -f2-|tr -d '"'); do
+for u in $(grep '<link.*http' "${TARGET}/index.html"|grep -v '<!--.*<link'|grep -o 'href="http[^"]*"'|cut -d"=" -f2-|tr -d '"'); do
     BNU="$(basename "${u}")"
     rm -f "${TARGET}/${BNU}"
     ( cd "${TARGET}" && wget -q "${u}" )
     sed -i -e "s!${u}!${BNU}!g" "${TARGET}/index.html"
 done
+
+find "${TARGET}" -name "*~" -print0|xargs -0 rm
