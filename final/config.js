1e4803e412e71d51813f353df6c90b26bc8274c0
Uli Heller <uli.heller@daemons-point.com>
2024-03-31 07:46:12 +0200

    Vorbereitung: Herunterladen

--- final/config.js
+++ final/config.js
@@ -6,12 +6,20 @@ const config = {
     ],
     javascripts: [
         "prism-1.29.0.js",
-        "tabulator-6.1.min.js",    // Auskommentieren für HTML-Tabellen
+        //"https://unpkg.com/prismjs/components/prism-core.min.js",
+        //"https://unpkg.com/prismjs/plugins/autoloader/prism-autoloader.min.js",
+        //"https://unpkg.com/prismjs@1.29.0/components/prism-core.min.js",
+        //"https://unpkg.com/prismjs@1.29.0/plugins/autoloader/prism-autoloader.min.js",
+        "https://unpkg.com/tabulator-tables/dist/js/tabulator.min.js",
+        //"https://unpkg.com/tabulator-tables@6.2.0/dist/css/tabulator.min.css",
     ],
     stylesheets: [
         "stuttgart.css",
         "prism-1.29.0.css",
-        "tabulator-6.1.min.css",   // Auskommentieren für HTML-Tabellen
+        //"https://unpkg.com/prismjs/themes/prism.css",
+        //"https://unpkg.com/prismjs@1.29.0/themes/prism.css",
+        "https://unpkg.com/tabulator-tables/dist/css/tabulator.min.css",
+        //"https://unpkg.com/tabulator-tables@6.2.0/dist/js/tabulator.min.js",
     ],
     multiLanguage: false,
     navbarClass:   'navbar',
@@ -23,14 +31,14 @@ const config = {
     timestamp:     '2024-03-29 07:00:02',
 
     tabulatorOptions: {
-	//height: "500px",         // Scrollbalken und "stehender" Tabellenkopf
-	layout: "fitDataStretch",  // Vermeidung von Leerspalte rechts
-	columnDefaults: {
-	    formatter:    "html",  // funktionierende HTML-Links
-	    headerFilter: "input", // Filterzeile
-	},
-	movableColumns: true,      // Sortierung der Spalten änderbar
-	movableRows: true,         // Sortierung der Zeilen änderbar
-	//spreadsheet: true,
+        //height: "500px",         // Scrollbalken und "stehender" Tabellenkopf
+        layout: "fitDataStretch",  // Vermeidung von Leerspalte rechts
+        columnDefaults: {
+            formatter:    "html",  // funktionierende HTML-Links
+            headerFilter: "input", // Filterzeile
+        },
+        movableColumns: true,      // Sortierung der Spalten änderbar
+        movableRows: true,         // Sortierung der Zeilen änderbar
+        //spreadsheet: true,
     },
 }
