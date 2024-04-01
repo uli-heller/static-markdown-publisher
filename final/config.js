1b57c610da2d2ec164477f072df40bb3709a0160
Uli Heller <uli.heller@daemons-point.com>
2024-04-02 01:02:23 +0200

    Tabulator via HTML-Tags laden, nicht via config.js

--- final/config.js
+++ final/config.js
@@ -5,20 +5,20 @@ const config = {
         { filename: "footer.md", elementId: "footermd", insertBeforeElementId: "bottomid", isNavbar: true },
     ],
     javascripts: [
-        "prism-1.29.0.js",
+        "prism-1.29.0.js", // prism.js kann nicht von unpkg heruntergeladen werden wegen Plugins
         //"https://unpkg.com/prismjs/components/prism-core.min.js",
         //"https://unpkg.com/prismjs/plugins/autoloader/prism-autoloader.min.js",
         //"https://unpkg.com/prismjs@1.29.0/components/prism-core.min.js",
         //"https://unpkg.com/prismjs@1.29.0/plugins/autoloader/prism-autoloader.min.js",
-        "https://unpkg.com/tabulator-tables/dist/js/tabulator.min.js",
+        //"https://unpkg.com/tabulator-tables/dist/js/tabulator.min.js",
         //"https://unpkg.com/tabulator-tables@6.2.0/dist/css/tabulator.min.css",
     ],
     stylesheets: [
         "stuttgart.css",
-        "prism-1.29.0.css",
+        "prism-1.29.0.css", // prism.js kann nicht von unpkg heruntergeladen werden wegen Plugins
         //"https://unpkg.com/prismjs/themes/prism.css",
         //"https://unpkg.com/prismjs@1.29.0/themes/prism.css",
-        "https://unpkg.com/tabulator-tables/dist/css/tabulator.min.css",
+        //"https://unpkg.com/tabulator-tables/dist/css/tabulator.min.css",
         //"https://unpkg.com/tabulator-tables@6.2.0/dist/js/tabulator.min.js",
     ],
     multiLanguage: false,
